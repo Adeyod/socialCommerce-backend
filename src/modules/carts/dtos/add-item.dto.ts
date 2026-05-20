08bb471c9114277eb4c719b1b1751dd4dd2e4822
Ayodeji Adebolu <ayodejiadebolu@gmail.com>
2026-05-20 21:25:41 +0100

    Fix: Error adding to cart

--- src/modules/carts/dtos/add-item.dto.ts
+++ src/modules/carts/dtos/add-item.dto.ts
@@ -1,16 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsString, Min } from 'class-validator';
 
 export class AddItemToCartDto {
   @ApiProperty({
     description: 'This is the ID of the product to be added.',
     example: '1owuenfue39485jrwhw9394',
   })
+  @IsString()
   productId!: string;
 
   @ApiProperty({
     description: 'This is the ID of the business that owns the product.',
     example: '1owuenfue39485jrwhw9394',
   })
+  @IsString()
   businessId!: string;
 
   @ApiProperty({
@@ -18,5 +21,7 @@ export class AddItemToCartDto {
       'This is the quantity of the product that the buyer is adding to cart.',
     example: 5,
   })
+  @IsNumber()
+  @Min(1)
   quantity!: number;
 }
